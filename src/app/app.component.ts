558f3a6fff77368e0988a6adc1d848264e5a5866
Daniella Clemmensen <dani7135@edu.ucl.dk>
2021-06-22 22:16:28 +0200

    Static chart

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chartProject';
+
+  chartOptions ={
+response: true
+  }
+  labels =  ['JAN', 'FEB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC'];
+ //Static
+  chartData = [
+    {
+      label: '1st Year',
+      data: [21, 56, 4, 31, 45, 15, 57, 61, 9, 17, 24, 59] 
+    },
+    { 
+      label: '2nd Year',
+      data: [47, 9, 28, 54, 77, 51, 24]
+    }
+  ];
+  colors = [
+    { // 1st Year.
+      backgroundColor: 'rgba(77,83,96,0.2)'
+    },
+    { // 2nd Year.
+      backgroundColor: 'rgba(30, 169, 224, 0.8)'
+    }
+  ]
+  onChartClick(event) {
+    console.log(event);
+  }
+  
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChartsModule } from 'ng2-charts';
 
+import {HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    ChartsModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
